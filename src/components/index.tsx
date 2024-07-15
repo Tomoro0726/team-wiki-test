ba3de55a0e01eb21b7740f218559fc28d61b55a2
Tomoro0726 <tomoro0726@gmail.com>
2024-07-15 18:32:25 +0900

    layout update

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -10,10 +10,10 @@ export default function Index(index: any) {
         indexList.map((item: any, index: any) => (
           <div key={index} className={`flex items-start ml-[2vw] h-min`}>
             <div className="flex justify-center [flex-flow:column] items-center align-middle">
-              <img className="w-[1.4vw] h-[1.4vw]" src={"/markdown/index_" + item.depth + ".svg"} />
-              <img src="/markdown/line.svg" className="w-[2px]" />
+              <img className="w-[1.4vw] h-[1.4vw]" src={"/markdown/index_3.svg"} />
+              <img src="/markdown/line.svg" className="h-[10px] w-[2px]" />
           </div>
-            <a className="ml-[9px] no-underline text-[1vw]" href={"#" + item.slug}>{item.text}</a>
+            <a className="ml-[9px] no-underline text-[0.9vw]" href={"#" + item.slug}>{item.text}</a>
         </div>
         ))
       }
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,16 +1,15 @@
 export default function Menu() {
   return (
-    <div className="flex justify-between items-center bg-[#ecf0f3] font-bold z-50">
+    <div className="flex justify-between items-center bg-[#ECF0f3] p-4 font-bold z-50">
       <a className="flex items-center w-1/4 pt-[0.5vw] pl-[2vw] pb-[0.5vw]" href="/">
-        <img src="/logo.png" className="w-[4.5vw] h-[4.5vw]" />
-        <p className="ml-[15px] text-[1.5vw] text-[#4D4D4D ] font-bold">iGEM Grand Tokyo</p>
+        <img src="/logo.webp" className="h-[3vw]" />
       </a>
-      <div className="flex items-center pt-[0.5vw] pr-[3vw]">
-        <a className="no-underline text-[#4D4D4D] text-[1.5vw] mr-[3vw]" href="/team">TEAM</a>
-        <a className="no-underline text-[#4D4D4D] text-[1.5vw] mr-[3vw]" href="/project">PROJECT</a>
-        <a className="no-underline text-[#4D4D4D] text-[1.5vw] mr-[3vw]" href="/weblab">WET LAB</a>
-        <a className="no-underline text-[#4D4D4D] text-[1.5vw] mr-[3vw]" href="/public/team.html">DRY LAB</a>
-        <a className="no-underline text-[#4D4D4D] text-[1.5vw] mr-[3vw]" href="/public/contact.html">HUMAN PRACTICE</a>
+      <div className="flex items-center pt-[0.5vw] pr-[1vw]">
+        <a className="no-underline text-[#4D4D4D] text-[1.3vw] mr-[3vw]" href="/team">TEAM</a>
+        <a className="no-underline text-[#4D4D4D] text-[1.3vw] mr-[3vw]" href="/project">PROJECT</a>
+        <a className="no-underline text-[#4D4D4D] text-[1.3vw] mr-[3vw]" href="/weblab">WET</a>
+        <a className="no-underline text-[#4D4D4D] text-[1.3vw] mr-[3vw]" href="/public/team.html">DRY</a>
+        <a className="no-underline text-[#4D4D4D] text-[1.3vw] mr-[3vw]" href="/public/contact.html">HUMAN PRACTICE</a>
       </div>
     </div>
   );
