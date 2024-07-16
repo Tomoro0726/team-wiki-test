717680be567e4e65de1c75eb269e0b4d087ba15a
Tomoro0726 <tomoro0726@gmail.com>
2024-07-16 11:47:35 +0900

    画像の設定を変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,18 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import remarkBreaks from 'remark-breaks';
 import rehypeKatex from 'rehype-katex'; // relevant
-import remarkMath from 'remark-math';   // relevant
+import remarkMath from 'remark-math'; // relevant
+
+import rehypeImg2pic from "astro-rehype-img2pic";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx({
-    remarkPlugins: [remarkMath], // relevant
+    remarkPlugins: [remarkMath],
+    // relevant
     rehypePlugins: [rehypeKatex] // relevant
-  })],
+  }), rehypeImg2pic()],
   markdown: {
-    remarkPlugins: [remarkBreaks,],
+    remarkPlugins: [remarkBreaks]
   }
 });
