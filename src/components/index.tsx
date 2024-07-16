2ef227ae103b693046e3dd4ecea71e4473b4b6b0
Tomoro0726 <tomoro0726@gmail.com>
2024-07-16 10:21:12 +0900

    最後の棒問題を修正

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,6 +4,10 @@ export default function Index(index: any) {
     indexList[i].text = indexList[i].text.replace(/<[^>]*>?/gm, '');
   }
   indexList = indexList.filter((item: any) => item.depth !== 4);
+  //配列の最後だけを取得
+  let last = indexList[indexList.length - 1];
+  //配列の最後を削除
+  indexList.pop();
   return (
     <div>
       {indexList.map((item: any, index: any) => (
@@ -23,6 +27,17 @@ export default function Index(index: any) {
           </a>
         </div>
       ))}
+      <div key={index} className={`flex items-start ml-[1.7vw] h-min`}>
+        <div className="flex justify-center [flex-flow:column] items-center align-middle">
+          <img className="w-[1.1vw] h-[1.1vw]" src={'/markdown/index_3.svg'} />
+        </div>
+        <a
+          className="ml-[9px] font-[600]	text-[#7a7c7d] no-underline text-[1vw]"
+          href={'#' + last.slug}
+        >
+          {last.text}
+        </a>
+      </div>
     </div>
   );
 }
