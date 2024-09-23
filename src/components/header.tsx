10089cbb391391b2e9f5fbfd66657319726037cc
Tomoro0726 <tomoro0726@gmail.com>
2024-09-23 22:28:07 +0900

    headerの挙動を修正

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,66 +1,81 @@
 export default function Header() {
-	const headers: { [header_name: string]: Array<string> } = {
-		TEAM: ["Team", "Attributions"],
-		PROJECT: ["Contribution", "Description"],
-		WET: [
-			"Plant",
-			"Safety",
-			"Experiments",
-			"Notebook",
-			"Results",
-			"Engineering",
-		],
-		DRY: ["Hardware", "Measurement", "Model", "Software"],
-		SAFETY: ["SAFETY"],
-		"HUMAN-PRACTICES": [
-			"Human-practices",
-			"Education",
-			"Entrepreneurship",
-			"Inclusivity",
-			"Sustainable",
-		],
-	};
+  const headers: { [header_name: string]: Array<string> } = {
+    TEAM: ["Team", "Attributions"],
+    PROJECT: ["Contribution", "Description"],
+    WET: [
+      "Plant",
+      "Safety",
+      "Experiments",
+      "Notebook",
+      "Results",
+      "Engineering",
+    ],
+    DRY: ["Hardware", "Measurement", "Model", "Software"],
+    SAFETY: ["SAFETY"],
+    "HUMAN-PRACTICES": [
+      "Human-practices",
+      "Education",
+      "Entrepreneurship",
+      "Inclusivity",
+      "Sustainable",
+    ],
+  };
 
-	return (
-		<nav
-			id="menu"
-			className="nav-menu flex justify-between items-center bg-[#ECF0f3] p-4 z-50 font-semibold"
-		>
-			<a
-				className="flex items-center w-1/4 pt-[0.5vw] pl-[2vw] pb-[0.5vw]"
-				href="/"
-			>
-				<img src="/logo.webp" className="h-[3vw]" />
-			</a>
-			<ul className="menu-list">
-				<div className="menu-item">
-					<a href="/">HOME</a>
-				</div>
+  return (
+    <nav
+      id="menu"
+      className="nav-menu flex justify-between items-center bg-[#ECF0f3] p-4 z-50 font-semibold"
+    >
+      <a
+        className="flex items-center w-1/4 pt-[0.5vw] pl-[2vw] pb-[0.5vw]"
+        href="/"
+      >
+        <img src="/logo.webp" className="h-[3vw]" />
+      </a>
+      <ul className="menu-list">
+        <div className="menu-item">
+          <a href="/" className="font-extrabold">
+            HOME
+          </a>
+        </div>
 
-				{Object.keys(headers).map((header) => (
-					//サブヘッダが複数ある時はdrop-menuクラスを追加
-					<div
-						className={`menu-item ${headers[header].length > 1 ? "drop-menu" : ""}`}
-					>
-						{headers[header].length > 1 ? ( // 親ヘッダにサブヘッダが複数ある時
-							<>
-								<a href={`/${header.toLowerCase()}`} className="font-extrabold">{header.toUpperCase()} &or;</a>
-								{/* URLはLowerで、headerはUpperで */}
-								<div className="drop-menu-list">
-									{headers[header].map((subHeader) => (
-										<div className="drop-menu-item">
-											<a href={`/${subHeader.toLowerCase()}`} className="font-semibold">{subHeader.charAt(0).toUpperCase() + subHeader.slice(1).toLowerCase()}</a>
-										</div>
-									))}
-								</div>
-							</>
-						) : (
-							// 親ヘッダにサブヘッダが1つの時
-							<a href={`/${headers[header][0].toLowerCase}`}>{headers[header][0].toUpperCase()}</a>
-						)}
-					</div>
-				))}
-			</ul>
-		</nav>
-	);
+        {Object.keys(headers).map((header) => (
+          //サブヘッダが複数ある時はdrop-menuクラスを追加
+          <div
+            className={`menu-item ${headers[header].length > 1 ? "drop-menu" : ""}`}
+          >
+            {headers[header].length > 1 ? ( // 親ヘッダにサブヘッダが複数ある時
+              <>
+                <a href={`/${header.toLowerCase()}`} className="font-extrabold">
+                  {header.toUpperCase()} &or;
+                </a>
+                {/* URLはLowerで、headerはUpperで */}
+                <div className="drop-menu-list">
+                  {headers[header].map((subHeader) => (
+                    <div className="drop-menu-item">
+                      <a
+                        href={`/${subHeader.toLowerCase()}`}
+                        className="font-semibold"
+                      >
+                        {subHeader.charAt(0).toUpperCase() +
+                          subHeader.slice(1).toLowerCase()}
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              </>
+            ) : (
+              // 親ヘッダにサブヘッダが1つの時
+              <a
+                className="font-extrabold"
+                href={`/${headers[header][0].toLowerCase()}`}
+              >
+                {headers[header][0].toUpperCase()}
+              </a>
+            )}
+          </div>
+        ))}
+      </ul>
+    </nav>
+  );
 }
