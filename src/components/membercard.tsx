5fc406f8165dc39ba80dec4b7e2e05790cbe1280
Tomoro0726 <tomoro0726@gmail.com>
2024-09-23 21:45:06 +0900

    add membercard

--- src/components/membercard.tsx
+++ src/components/membercard.tsx
@@ -1,12 +1,25 @@
-export default function MemberCard(contributor: any) {
-  console.log(contributor);
+import { useState } from "react";
+
+interface ContributorProps {
+  contributor: string;
+}
+
+export default function MemberCard({ contributor }: ContributorProps) {
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <div>
       <img
-        src={`/public/contributors/${contributor.contributor}.webp`}
-        alt={contributor.contributor}
+        src={`/contributors/${contributor}.webp`}
+        alt={contributor}
         className="w-[20vw] h-[20vw] rounded-[50%]"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       />
+      {isHovered && (
+        <div className="absolute top-[20vw] left-[20vw] w-[20vw] h-[20vw] bg-[#c6dff4] p-[1vw] rounded-[10%]">
+          {contributor}
+        </div>
+      )}
     </div>
   );
 }
