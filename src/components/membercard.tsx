090a60ef1141d1c8ec829763fe72e645a1311b5f
Tomoro0726 <tomoro0726@gmail.com>
2024-09-28 19:13:09 +0900

    名前のCSS対応

--- src/components/membercard.tsx
+++ src/components/membercard.tsx
@@ -4,12 +4,13 @@ interface ContributorProps {
 
 export default function MemberCard({ contributor }: ContributorProps) {
   return (
-    <div className="cursor-pointer">
+    <div className="cursor-pointer text-center">
       <img
         src={`/contributors/${contributor}.webp`}
         alt={contributor}
         className="w-[20vw] h-[20vw] rounded-[50%]"
       />
+      <h3 className="text-2xl font-light">Tomoro Saito</h3>
     </div>
   );
 }
