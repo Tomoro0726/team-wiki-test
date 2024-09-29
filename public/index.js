97a813cbede9fdd93c6b514f34387339332e6447
Tomoro0726 <tomoro0726@gmail.com>
2024-09-29 16:58:41 +0900

    戻る

--- public/index.js
+++ public/index.js
@@ -2,18 +2,22 @@ console.log("Hello from public/index.js!");
 window.addEventListener("scroll", () => {
   const scrollPosition = window.scrollY;
   const viewportHeight = window.innerHeight;
-  const threshold = 0.15 * viewportHeight; // 30vh
+  const threshold = 0.1 * viewportHeight; // 30vh
 
   const topImage = document.querySelector("#top-image-1");
+  //top:0に値を追加
+  //30vhのpx値を取得
+  let topImageTop = window.getComputedStyle(topImage).top;
+  topImageTop = parseInt(topImageTop, 10);
+
   //アニメーションしながら消える
   if (scrollPosition > threshold) {
-    //徐々に回転する
-    topImage.style.transform =
-      "rotate(" + ((scrollPosition - threshold) / threshold) * -20 + "deg)";
+    topImage.style.top = topImageTop + scrollPosition * +1.5 + "px";
     //徐々に透明にする
     topImage.style.opacity = 1 - (scrollPosition - threshold) / threshold;
   } else {
-    //透明度を元に戻す
+    //top:30vhに戻す
+    topImage.style.top = "30vh";
     topImage.style.opacity = 1;
   }
 });
