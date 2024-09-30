7d37dcf7ec913cbdc10644a785c93dafcdcc8191
gorira <tatsuhiko.shigoto@gmail.com>
2024-09-30 13:47:20 +0900

    とりあえずmemberpageの必要なところだけ

--- public/memberCardHover.js
+++ public/memberCardHover.js
@@ -1,95 +1,107 @@
 memberCardData = {
-  contributor0: {
-    name: "Shiori Kajikawa",
-    explanation: `I'm Shiori, an aspiring scientist continuously fascinated by the wonders of biotechnology. My experience of leading this team has transformed by life; I am truly thankful for the support all has given me. I am really, really glad I did iGEM, and I hope you enjoy our wiki! 
-`,
+  "contributor0": {
+    "name": "Shiori Kajikawa",
+    "role": "Wet",
+    "image": "Shiori_Kajikawa",
+    "explanation": "I'm Shiori, an aspiring scientist continuously fascinated by the wonders of biotechnology. My experience of leading this team has transformed my life; I am truly thankful for the support all has given me. I am really, really glad I did iGEM, and I hope you enjoy our wiki!"
   },
-  contributor1: {
-    name: "Kei Hato",
-    explanation:
-      "I am very honored to have the support of so many people and to be able to work with a wide variety of colleagues. I want to rush all the way to the grand jamboree!",
+  "contributor11": {
+    "name": "Tatsuhiko Akiyama",
+    "role": "Wiki",
+    "image": "Tatsuhiko_Akiyama",
+    "explanation": "We are able to function as a team like this because of the people around us who support us, and we will do our best to proudly bring home the Grand Prize."
   },
-  contributor2: {
-    name: "Arisa Tani",
-    explanation: `I truly enjoy working with these awesome and unique members. In the future, I hope to excel at research and become an entrepreneur; I want to step closer to that dream through iGEM!`,
+  "contributor9": {
+    "name": "Ayaka Kotajima",
+    "role": "Dry",
+    "image": "Ayaka_Kotajima",
+    "explanation": "I am grateful to have this opportunity while still a high school student, and I will do my best in my research. Thank you!"
   },
-  contributor3: {
-    name: "Saki Tsuchiya",
-    explanation: `This is Saki Tsuchiya, who will take the part in wet lab and education activities.
-“I will work hard to win the prize in Education!!”`,
+  "contributor16": {
+    "name": "Mizuho Sakai",
+    "role": "Human Practices",
+    "image": "Mizuho_Sakai",
+    "explanation": "I will do my best to utilize the experience I have gained so far to further my own growth, so I appreciate your continued support."
   },
-  contributor4: {
-    name: "Contributor 4",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 4.",
+  "contributor2": {
+    "name": "Arisa Tani",
+    "role": "Wet/Education",
+    "image": "Arisa_Tani",
+    "explanation": "I truly enjoy working with these awesome and unique members. In the future, I hope to excel at research and become an entrepreneur; I want to step closer to that dream through iGEM!"
   },
-  contributor5: {
-    name: "Contributor 5",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 5.",
+  "contributor12": {
+    "name": "Wingdor Doria Lee",
+    "role": "Human Practices/Education/Wet",
+    "image": "Wingdor_Doria_Lee",
+    "explanation": "As a member of Grand Tokyo, I will take on the Grand Jamboree with a sense of responsibility and with the support of everyone."
   },
-  contributor6: {
-    name: "Contributor 6",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 6.",
+  "contributor4": {
+    "name": "Shoya Inoue",
+    "role": "Wet",
+    "image": "Shoya_Inoue",
+    "explanation": "I will continue to do my best, so I look forward to your continued support."
   },
-  contributor7: {
-    name: "Contributor 7",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 7.",
+  "contributor7": {
+    "name": "Miruhi Tsumiyama",
+    "role": "Human Practices",
+    "image": "Miruhi_Tsumiyama",
+    "explanation": "We will do our best to win the Grand Prize while carrying the feelings of those who support GrandTokyo on our backs."
   },
-  contributor8: {
-    name: "Contributor 8",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 8.",
+  "contributor1": {
+    "name": "Kei Hato",
+    "role": "Wet",
+    "image": "Kei_Hato",
+    "explanation": "I am very honored to have the support of so many people and to be able to work with a wide variety of colleagues. I want to rush all the way to the grand jamboree!"
   },
-  contributor9: {
-    name: "Contributor 9",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 9.",
+  "contributor6": {
+    "name": "Rico Kasai",
+    "role": "Human Practices",
+    "image": "Rico_Kasai",
+    "explanation": "I learned a lot from encountering so many members! I will enjoy it to the end :)"
   },
-  contributor10: {
-    name: "Contributor 10",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 10.",
+  "contributor3": {
+    "name": "Saki Tsuchiya",
+    "role": "Human Practices/Education/Wet",
+    "image": "Saki_Tsuchiya",
+    "explanation": "I will work hard to win the prize in Education!!"
   },
-  contributor11: {
-    name: "Contributor 11",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 11.",
+  "contributor15": {
+    "name": "Rikuto Fukushima",
+    "role": "Education/Wet",
+    "image": "Rikuto_Fukushima",
+    "explanation": "I am truly happy to have had the opportunity to satisfy my intellectual curiosity to this extent. I will do my best to win the Grand Prize!"
   },
-  contributor12: {
-    name: "Contributor 12",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 12.",
+  "contributor13": {
+    "name": "Tomoro Saito",
+    "role": "Wiki",
+    "image": "Tomoro_Saito",
+    "explanation": "I have a lot of fun working in an environment where I can work surrounded by members with advanced skills and knowledge that I wouldn't expect from high school students. I will do my best to aim for the Grand Prize."
   },
-  contributor13: {
-    name: "Contributor 13",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 13.",
+  "contributor14": {
+    "name": "Ryuzo Kijima",
+    "role": "Education/Wet",
+    "image": "Ryuzo_Kijima",
+    "explanation": "I'm happy to have met such interesting friends. We will do our best to win the Grand Prize."
   },
-  contributor14: {
-    name: "Contributor 14",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 14.",
+  "contributor5": {
+    "name": "Misaki Ozawa",
+    "role": "Wet",
+    "image": "Misaki_Ozawa",
+    "explanation": "I will use my experience to the fullest and do my best. I look forward to working with working with these members!"
   },
-  contributor15: {
-    name: "Contributor 15",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 15.",
+  "contributor10": {
+    "name": "Yukiya Horiba",
+    "role": "Human Practices/Education",
+    "image": "Yukiya_Horiba",
+    "explanation": "I will do my best to win the Grand Prize! Thank you for your support of Grand-Tokyo!!"
   },
-  contributor16: {
-    name: "Contributor 16",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 16.",
-  },
-  contributor17: {
-    name: "Contributor 17",
-    explanation:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quosblanditiis tenetur unde suscipit, quam beatae rerum inventoreconsectetur, neque doloribus, cupiditate numquam dignissimos laborumLorem ipsum dolor sit amet, consectetur adipisicing elit. Quos 16.",
-  },
+  "contributor8": {
+    "name": "Kan Soda",
+    "role": "Wiki",
+    "image": "Kan_Soda",
+    "explanation": "I'm really happy to be able to work in an environment surrounded by people with high aspirations. We will support you as much as possible in terms of design!"
+  }
 };
-
 //URLの#以降の文字列を取得
 const urlHash = location.hash;
 //URLの#以降の文字列を取得
